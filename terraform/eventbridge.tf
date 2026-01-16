def9c1e43121627541f35ca06d389f19552b6e45
Matthew  Black <matoblac@amazon.com>
2026-01-16 17:46:56 -0500

    added eventbridge.tf

---
+++ terraform/eventbridge.tf
@@ -0,0 +1,21 @@
+# eventbridge.tf 
+
+resource "aws_cloudwatch_event_rule" "backup_schedule" {
+    name              = "workspaces-backup-schedule"
+    description       = "Trigger WorkSpaces image creation"
+    schedule_expression = var.backup_schedule
+}
+
+resource "aws_cloudwatch_event_target" "lambda" {
+    rule    = aws_cloudwatch_event_rule.backup_schedule.name
+    target_id = "WorkspacesBackupLambda" 
+    arn     = "aws_lambda_function.workspaces_image_manager.arn"
+}
+
+resource "aws_lambda_permission" "allow_eventbridge" {
+    statement_id = "AllowExecutionFromEventBridge"
+    action       = "lambda:InvokeFunction"
+    function_name = aws_lambda_function.workspace_image_manager.function_name
+    principal = "events.amazonaws.com"
+    source_arn = aws_cloudwatch_event_rule.backup_schedule.arn
+}
